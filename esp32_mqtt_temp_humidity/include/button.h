e6ddb98ed60b038592d49b4210dffe690b59a0fe
Atharva Lele <itsatharva@gmail.com>
2022-02-16 22:07:02 -0800

    esp32: button: initial debouncing and task implementation
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

---
+++ esp32_mqtt_temp_humidity/include/button.h
@@ -0,0 +1,18 @@
+#ifndef _BUTTON_H_
+#define _BUTTON_H_
+
+/**
+ * button: implementation of button debouncing 
+ * and wrapper around Arduino's digitalRead()
+ * 
+ * Author: Atharva Lele <atharva@pdx.edu>
+ */
+
+#include <Arduino.h>
+
+/* Functions */
+void buttonSetup();
+bool buttonGetStatus();
+void buttonTask();
+
+#endif
---
+++ esp32_mqtt_temp_humidity/src/button.cpp
@@ -0,0 +1,51 @@
+/**
+ * button: implementation of button debouncing 
+ * and wrapper around Arduino's digitalRead()
+ * 
+ * Author: Atharva Lele <atharva@pdx.edu>
+ */
+
+#include "button.h"
+
+static bool buttonPressed = false;
+static bool lastButtonState = false;
+static uint8_t buttonPin = 21;
+static unsigned long pressDetectTime = 0;
+static uint8_t debounceTime = 25;
+
+/* Setup button pin mode */
+void buttonSetup()
+{
+    // Using INPUT_PULLDOWN since I don't have a
+    // pulldown resistor on the board
+    pinMode(buttonPin, INPUT_PULLDOWN);
+}
+
+/* Return button status */
+bool buttonGetStatus()
+{
+    return buttonPressed;
+}
+
+/* Button Task */
+void buttonTask()
+{
+    /* Read pin status */
+    bool pinVal = digitalRead(buttonPin);
+
+    /* Reset timer if button status changed */
+    if (pinVal != lastButtonState) {
+        pressDetectTime = millis();
+    }
+    
+    /* Button has stabilized, update status */
+    if ((millis() - pressDetectTime) > debounceTime) {
+        if (pinVal != buttonPressed) {
+            buttonPressed = pinVal;
+            Serial.printf("Button Press: %d\n", buttonPressed);
+        }
+    }
+
+    /* Update last state */
+    lastButtonState = pinVal;
+}
