157008680cb349ddedbf3c554a1392199173b7af
Atharva Lele <itsatharva@gmail.com>
2022-02-19 12:00:51 -0800

    esp32: main: initial implementation
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- esp32_mqtt_temp_humidity/src/main.cpp
+++ esp32_mqtt_temp_humidity/src/main.cpp
@@ -1,9 +1,69 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include "button.h"
+#include "mqtt.h"
+#include "rgb_led.h"
+#include "sensor.h"
+
+#define WIFI_SSID "WIFI_NAME"
+#define WIFI_PASS "WIFI_PASS"
+
+/* Connect to WiFi */
+void wifiConnect()
+{
+    /* Station Mode */
+    WiFi.mode(WIFI_STA);
+
+    Serial.println("Connecting to WiFi...");
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    while (WiFi.status() != WL_CONNECTED) {
+        LEDSetState(LED_R, 255);
+        LEDSetState(LED_B, 255);
+        delay(250);
+        LEDSetState(LED_R, 0);
+        LEDSetState(LED_B, 0);
+        delay(250);
+    }
+
+    /* Connected to Wifi, set LED to Cyan */
+    LEDSetState(LED_R, 0);
+    LEDSetState(LED_B, 255);
+    LEDSetState(LED_G, 255);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+    /* Setup serial connection */
+    Serial.begin(115200);
+
+    /* Setup LEDs, buttons, sensors */
+    buttonSetup();
+    if (sensorSetup())
+    {
+        Serial.println("Sensor Found!");
+    }
+    else
+    {
+        Serial.println("Sensor error!");
+        while (1); // Wait in error
+    }
+
+    /* RGB LED Setup */
+    LEDSetup();
+    /* On-board LED setup */
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    /* Setup WiFi */
+    wifiConnect();
+
+    /* MQTT Setup */
+    mqttSetup();
+}
+
+void loop()
+{
+    buttonTask();
+    sensorTask();
+    mqttTask();
 }
